6a676f1796acdcfc2c2e0c981813c72777069c56
Karthik0484 <karthikdeepshika484@gmail.com>
2024-09-21 14:30:22 +0530

    added upto button

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </div>
+
+  );
+};
+
+export default App;
